d8fbf12665e69fb746da9318a3895bc260c01f62
hongzhengong <hongzhengonghzg@gmail.com>
2018-07-06 16:12:01 +0800

    提交静态界面
    提交静态界面，首页，电影详情，后台录入页，后台list界面

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views/pages'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+// app.use(express.bodyParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
